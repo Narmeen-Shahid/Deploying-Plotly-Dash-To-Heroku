1bb4e49f25ccea4bbef9df34ead03b260706094c
Narmeen Shahid <narmeenshahid18@gmail.com>
2019-07-31 19:00:18 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,20 +1,22 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+from flask import Flask
+import os
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = Flask(__name__)
+server.secret_key = os.environ.get('secret_key', 'secret')
+app = dash.Dash(name = __name__, server = server)
+app.config.supress_callback_exceptions = True
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
-
     html.Div(children='''
         Dash: A web application framework for Python.
     '''),
-
     dcc.Graph(
-        id='example-graph',
+        id='example-graph',    
         figure={
             'data': [
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
@@ -24,8 +26,14 @@ app.layout = html.Div(children=[
                 'title': 'Dash Data Visualization'
             }
         }
-    )
+    ),
+    dcc.Input(id='my-id', value='initial value', type="text"),
+    html.Div(id='my-div')
 ])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
