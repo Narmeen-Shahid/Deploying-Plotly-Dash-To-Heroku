2af30dbbe3ca39fe789a97c93fd5d404d039679a
Narmeen Shahid <narmeenshahid18@gmail.com>
2019-08-01 22:46:21 +0500

    Update run.py

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ import dash_table
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div([
